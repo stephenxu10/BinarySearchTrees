bd6c7b478525d0c3f01564c1a5842480a7dc2d77
stephenxu10 <smx2@rice.edu>
2022-08-25 13:05:59 -0500

    Initial commit

---
+++ BST.py
@@ -0,0 +1,11 @@
+"""
+Implementation of a Binary Search Tree with the TreeNode class defined with parent, left child, right child, and key
+value.
+    - parent is the pointer to the parent node of each TreeNode.
+    - left  is the pointer to the left child of each TreeNode
+    - right is the pointer to the right child of each TreeNode.
+    - key is the value of the TreeNode.
+
+Consists of the following methods:
+    - insert(x): inserts the key x into the tree.
+"""
