e734cb89bae9ca34f152a39bb1ffe3d5cb9b1002
stephenxu10 <smx2@rice.edu>
2022-08-25 13:15:38 -0500

    Skeleton of the Class

--- BST.py
+++ BST.py
@@ -8,4 +8,27 @@ value.
 
 Consists of the following methods:
     - insert(x): inserts the key x into the tree.
+    - delete(x): deletes the node x from the tree and adjusts the tree accordingly.
+    - search(x): searches the tree for node x, returns -1 if not found.
+    - inorder(): performs an inorder traversal of the BST.
 """
+
+
+class BST:
+    def __init__(self, key=None, left=None, right=None, parent=None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.parent = None
+
+    def insert(self, node):
+        pass
+
+    def delete(self, node):
+        pass
+
+    def search(self, node):
+        pass
+
+    def inorder(self):
+        pass
