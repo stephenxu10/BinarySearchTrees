a8d4d43e75d902560bdeda925e35a4dd9f03c998
stephenxu10 <smx2@rice.edu>
2022-08-25 21:00:52 -0500

    Added all methods except delete

--- BST.py
+++ BST.py
@@ -1,34 +1,109 @@
 """
-Implementation of a Binary Search Tree with the TreeNode class defined with parent, left child, right child, and key
+Implementation of a Binary Search Tree with the Node class defined with parent, left child, right child, and key
 value.
     - parent is the pointer to the parent node of each TreeNode.
     - left  is the pointer to the left child of each TreeNode
     - right is the pointer to the right child of each TreeNode.
     - key is the value of the TreeNode.
 
+ASSUMES THE VALUES IN THE BST ARE UNIQUE.
 Consists of the following methods:
     - insert(x): inserts the key x into the tree.
+    - serialize(): performs a serialized preorder traversal on the tree.
     - delete(x): deletes the node x from the tree and adjusts the tree accordingly.
     - search(x): searches the tree for node x, returns -1 if not found.
-    - inorder(): performs an inorder traversal of the BST.
+    - inorder(): performs  and returns tje inorder traversal of the BST.
 """
 
 
-class BST:
+class Node:
     def __init__(self, key=None, left=None, right=None, parent=None):
         self.key = key
         self.left = left
         self.right = right
-        self.parent = None
+        self.parent = parent
 
-    def insert(self, node):
-        pass
+
+class BST:
+    def __init__(self, root=None):
+        self.root = root
+
+    def __str__(self):
+        return self.serialize()
+
+    def serialize(self):
+        root = self.root
+
+        def recurse(root, string):
+            """ a recursive helper function for the serialize() function."""
+            # check base case
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.key) + ','
+                string = recurse(root.left, string)
+                string = recurse(root.right, string)
+            return string
+
+        serial = recurse(root, '')
+
+        if serial[-1] == ",":
+            serial = serial[:-1]
+
+        return serial
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+
+        else:
+            data = Node(data)
+            x = self.root
+            prev = None
+
+            while x:
+                prev = x
+
+                if data.key < x.key:
+                    x = x.left
+
+                else:
+                    x = x.right
+
+            data.parent = prev
+
+            if data.key < prev.key:
+                prev.left = data
+            else:
+                prev.right = data
 
     def delete(self, node):
         pass
 
-    def search(self, node):
-        pass
+    def search(self, target):
+        """
+        returns the pointer to node in the BST. Returns None if not found.
+        """
+        root = self.root
+
+        while root and target != root.key:
+            if target < root.key:
+                root = root.left
+
+            else:
+                root = root.right
+
+        return BST(root)
 
     def inorder(self):
-        pass
+        root = self.root
+        order = []
+
+        def helper(root, order):
+            if root:
+                helper(root.left, order)
+                order.append(root.key)
+                helper(root.right, order)
+
+        helper(root, order)
+        return order
---
+++ tester.py
@@ -0,0 +1,16 @@
+from BST import BST
+
+tree = BST()
+tree.insert(15)
+tree.insert(6)
+tree.insert(3)
+tree.insert(7)
+tree.insert(18)
+tree.insert(17)
+tree.insert(20)
+tree.insert(2)
+tree.insert(4)
+tree.insert(13)
+tree.insert(9)
+print(tree)
+print(tree.search(13))
